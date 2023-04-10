db082d19b8f6ddad6a9d690ecff53cd9f3c1d4fa
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2023-04-10 16:31:35 +0200

    init

---
+++ KlinikaPlusWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KlinikaPlusWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
